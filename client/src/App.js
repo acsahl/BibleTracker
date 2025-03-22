de659b42b997f5e5b118c901dd416fbc75f64109
acsahl <90639690+acsahl@users.noreply.github.com>
2025-03-22 12:27:06 -0400

    Switch to BrowserRouter for GitHub Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import DevotionalCalendar from './components/Calendar';
