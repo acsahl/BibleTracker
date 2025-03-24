4d8a00bb260db2d1d7b9673d959ccb5228f7b818
acsahl <90639690+acsahl@users.noreply.github.com>
2025-03-24 16:46:58 -0400

    Fix ESLint warnings in components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
   const handleLogout = () => {
     // Clear all authentication data
     localStorage.removeItem('token');
