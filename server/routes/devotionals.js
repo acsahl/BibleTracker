b43b4d82367b0818f3964071c552aa2e920f8fa3
acsahl <90639690+acsahl@users.noreply.github.com>
2025-03-27 19:57:30 -0400

    Fix date handling in devotional creation to prevent date shifting

--- server/routes/devotionals.js
+++ server/routes/devotionals.js
@@ -65,7 +65,8 @@ router.post('/', verifyToken, async (req, res) => {
     
     // Parse the date string and create a date object
     const [year, month, day] = date.split('-').map(Number);
-    const devotionalDate = new Date(year, month - 1, day);
+    const devotionalDate = new Date(Date.UTC(year, month - 1, day));
+
     
     const devotional = new Devotional({
       date: devotionalDate,
