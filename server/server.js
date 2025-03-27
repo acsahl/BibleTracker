261c9d5b9d0ca7690c71d2b727147f5d05bbaa69
acsahl <90639690+acsahl@users.noreply.github.com>
2025-03-27 17:58:38 -0400

    Update CORS configuration and environment settings

--- server/server.js
+++ server/server.js
@@ -20,7 +20,8 @@ app.use((req, res, next) => {
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3003',
-  'https://saiv-biblestudy.netlify.app'
+  'https://saiv-biblestudy.netlify.app',
+  'https://bibletracker-1.onrender.com'
 ];
 
 // Basic CORS middleware
@@ -30,7 +31,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
   res.setHeader('X-Content-Type-Options', 'nosniff');
@@ -43,7 +44,7 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
 
 app.use(express.json());
